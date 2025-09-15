2fefe1cfe441f45da99a060f5f471ac871f2172c
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-09-15 14:06:21 +0700

    update MahasiswaModel.php

--- CI4 - Mission3/app/Models/mahasiswamodel.php
+++ CI4 - Mission3/app/Models/mahasiswamodel.php
@@ -3,19 +3,17 @@
 namespace App\Models;
 use CodeIgniter\Model;
 
-class mahasiswamodel extends Model
+class MahasiswaModel extends Model
 {
     protected $table = 'mahasiswa';
 
-    public function getmahasiswa()
+    public function getMahasiswa()
     {
-        // Kode ini sudah benar untuk mengambil data dari database
         return $this->findAll();
     }
 
     public function getMahasiswaByNim($nim)
     {
-        // Cari data berdasarkan kolom 'nim' dan ambil baris pertama
         return $this->where(['nim' => $nim])->first();
     }
 }
