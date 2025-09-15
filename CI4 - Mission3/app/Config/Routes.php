d60f281ee2ec9ff9382426fa25cc80eccc2ce492
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-09-15 20:19:13 +0700

    add routes fot login

--- CI4 - Mission3/app/Config/Routes.php
+++ CI4 - Mission3/app/Config/Routes.php
@@ -8,4 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('home', 'Home::index');
 $routes->get('mahasiswa', 'mahasiswa::index');
 $routes->get('mahasiswa/detail/(:num)', 'mahasiswa::detail/$1');
+$routes->get('/login', 'Auth::login');
+$routes->post('/login/process', 'Auth::loginProcess');
+$routes->get('/logout', 'Auth::logout');
 
